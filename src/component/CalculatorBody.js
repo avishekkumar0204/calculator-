e2a2c881273317a7d8546ba077fad4bdaa5d02be
Kumar <ar-avishek.kumar@p29804.intra.rakuten.co.jp>
2022-03-22 15:27:51 +0530

    heading and body created

---
+++ src/component/CalculatorBody.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function CalculatorBody() {
+  return (
+    <div className='border-2 my-4 text-center'>CalculatorBody</div>
+  )
+}
---
+++ src/component/Heading.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Heading(props) {
+  return (
+    <div className=" text-center text-5xl font-mediumpy-2">{props.title}</div>
+  )
+}
